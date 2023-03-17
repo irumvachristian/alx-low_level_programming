4dc7f86b6b2579efd732149bfe7fb2d3a162b53d
irumvachristian <irumvachristian3@gmail.com>
2023-03-17 03:15:14 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is main function
+ *
+ * Return: always 0
+ */
+int main()
+{
+printif("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
